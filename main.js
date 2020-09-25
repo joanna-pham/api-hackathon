6f3c77a125313c4077557fe99306b9ae1c380a28
Joanna Pham <jopham90@gmail.com>
2020-09-25 10:20:43 -0700

    rendered data for image

--- main.js
+++ main.js
@@ -1,68 +1,92 @@
-// //pass data in global scope
-// var movieArray
+//pass data in global scope
+var movieArray;
+var container = document.querySelector('container');
 
-// var btnHomePage = document.getElementById('btnHomePage');
-// btnHomePage.addEventListener('click', handleClick);
+var btnHomePage = document.getElementById('btnHomePage');
+btnHomePage.addEventListener('click', handleClick);
 
-// function handleClick(e){
+function handleClick(e) {
+}
 
-// }
 
-
-// //The Movie Database API
-// $.ajax({
-//   url: 'https://api.themoviedb.org/3/movie/top_rated?api_key=b67aeeb26d6866b85c4c2077bf8bbfc0&language=en-US&page=1',
-//   method: 'GET',
-//   success: function (data) {
-//     movieArray = data.results
-//     //passing data as an argument to the function
-//     renderData(data.results); //function with placeholder value
-//   },
-//   error: function (data) {
-//     console.log(data)
-//   }
-// })
-
-//Zomato
+//The Movie Database API
 $.ajax({
-  url: 'https://developers.zomato.com/api/v2.1/search',
+  url: 'https://api.themoviedb.org/3/movie/top_rated?api_key=b67aeeb26d6866b85c4c2077bf8bbfc0&language=en-US&page=1',
   method: 'GET',
-  header: {
-    "X-Access-Token": "fc53565b99be0fd264e83e23e8ca9552"
-  },
   success: function (data) {
-    console.log(data)
+    movieArray = data.results
+    //passing data as an argument to the function
+    // console.log(data.results)
+    renderData(data.results); //function with placeholder value
   },
   error: function (data) {
     console.log(data)
   }
 })
 
-// //define function has a function keyword
-
-// function renderData(){
-//   var randomMovie = Math.floor(Math.random() * movieArray.length)
-//   var poster
-//   for (var i = 0; i < movieArray.length; i++){
-//     randomMovie = movieArray[i]
+//Zomato
+// $.ajax({
+//   url: 'https://developers.zomato.com/api/v2.1/search?entity_id=92677',
+//   method: 'GET',
+//   headers: {
+//     "user-key": "fc53565b99be0fd264e83e23e8ca9552",
+//   },
+//   success: function (data) {
+//     console.log("ZOMATO", data)
+//   },
+//   error: function (data) {
+//     console.log(data)
 //   }
-//   console.log("link", randomMovie['poster_path'])
-
-//   randomPosterPath = randomMovie['poster_path'];
-//   randomMovieID = randomMovie['id'];
-//   console.log("randomMovieID", randomMovieID)
-
-//   var image = document.createElement('img');
-//   // image.src = 'https://api.themoviedb.org/3/movie/' + randomMovieID + randomPosterPath + ''
-//   image.src = 'https://api.themoviedb.org/3/movie/' + randomMovieID + '/images?api_key=b67aeeb26d6866b85c4c2077bf8bbfc0&language=en-US'
-
-//   console.log(image)
+// })
 
 
+//define function has a function keyword
+
+function renderData() {
+  //MOVIE INFO
+  var randomMovie = Math.floor(Math.random() * movieArray.length) //pick random number from length of array
+  // var poster
+  for (var i = 0; i < movieArray.length; i++) { //loop through movie array to match number with array items
+    randomMovie = movieArray[i]
+  }
+
+  var randomMovieTitle = randomMovie['original_title'] //title data
+  var randomPosterPath = randomMovie['poster_path']; //image path data
+  var randomMovieID = randomMovie['id']; //movie ID number
+
+  //MOVIE - Get Image
+  $.ajax({
+    url: 'https://api.themoviedb.org/3/configuration?api_key=b67aeeb26d6866b85c4c2077bf8bbfc0',
+    method: 'GET',
+    success: function (data) {
+      var imgUrl = data.images['base_url'];
+      var createImg = document.createElement('img');
+      createImg.src = imgUrl + 'w200' + randomPosterPath
+
+      // ---------------got image link, works... how do I make it appear on the screen
+    },
+    error: function (data) {
+      console.log(data)
+    }
+  })
 
 
-//   var btnGenerate = document.createElement('btn');
-//   btnGenerate.classList.add()
-// }
+  // var image = document.createElement('img');
+  // image.src = 'https://api.themoviedb.org/3/movie/' + randomMovieID + randomPosterPath + ''
+  // image.src = 'https://api.themoviedb.org/3/movie/' + randomMovieID + '/images?api_key=b67aeeb26d6866b85c4c2077bf8bbfc0&language=en-US'
 
-// //multiple math.random by length of the array
+  // var movieTitle = document.createElement('h3');
+  // movieTitle.textContent = randomMovieTitle;
+  // container.appendChild(movieTitle)
+
+
+  //ZOMATO INFO
+
+
+
+
+  var btnGenerate = document.createElement('btn');
+  btnGenerate.classList.add()
+}
+
+//multiple math.random by length of the array
